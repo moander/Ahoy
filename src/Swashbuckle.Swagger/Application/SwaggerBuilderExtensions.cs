00bf31052dd96916ac07bf2436a52fc198f0e8ee
moander <moander@outlook.com>
2016-10-26 10:18:05 +0200

    Allow routes without documentName

--- src/Swashbuckle.Swagger/Application/SwaggerBuilderExtensions.cs
+++ src/Swashbuckle.Swagger/Application/SwaggerBuilderExtensions.cs
@@ -7,15 +7,34 @@ namespace Microsoft.AspNetCore.Builder
 {
     public static class SwaggerBuilderExtensions
     {
+        private const string DefaultRouteTemplate = "swagger/{documentName}/swagger.json";
+
         public static IApplicationBuilder UseSwagger(
             this IApplicationBuilder app,
-            string routeTemplate = "swagger/{documentName}/swagger.json",
+            string routeTemplate = DefaultRouteTemplate,
             Action<SwaggerDocument, HttpRequest> documentFilter = null)
         {
-            return app.UseMiddleware<SwaggerMiddleware>(routeTemplate, documentFilter ?? PassThroughDocumentFilter);
+            return app.UseSwagger(options =>
+            {
+                options.RouteTemplate = routeTemplate ?? DefaultRouteTemplate;
+                options.DocumentFilter = documentFilter ?? PassThroughDocumentFilter;
+            });
+        }
+
+        public static IApplicationBuilder UseSwagger(this IApplicationBuilder app, Action<SwaggerMiddlewareOptions> setupAction)
+        {
+            var options = new SwaggerMiddlewareOptions
+            {
+                RouteTemplate = DefaultRouteTemplate,
+                DocumentFilter = PassThroughDocumentFilter,
+            };
+
+            setupAction(options);
+
+            return app.UseMiddleware<SwaggerMiddleware>(options);
         }
 
         private static void PassThroughDocumentFilter(SwaggerDocument swaggerDoc, HttpRequest httpRequest)
-        {}
+        { }
     }
 }
---
+++ src/Swashbuckle.Swagger/Application/SwaggerMiddlewareOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Swashbuckle.Swagger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swashbuckle.Swagger.Application
+{
+    public class SwaggerMiddlewareOptions
+    {
+        public Action<SwaggerDocument, HttpRequest> DocumentFilter { get; set; }
+
+        public string RouteTemplate { get; set; }
+
+        /// <summary>
+        /// Used when {documentName} is omitted in <see cref="RouteTemplate"/>
+        /// </summary>
+        public string DefaultDocumentName { get; set; }
+    }
+}
